a586174ac6d95af19136393dcf6233dbe1c27765
qwe5507 <apvmffkdls@gmail.com>
2023-01-14 17:34:52 +0900

    test : 적립금 적립 테스트 코드 추가

--- src/test/java/com/marketboro/point/service/ReservesServiceTest.java
+++ src/test/java/com/marketboro/point/service/ReservesServiceTest.java
@@ -1,9 +1,11 @@
 package com.marketboro.point.service;
 
+import com.marketboro.point.domain.history.History;
 import com.marketboro.point.domain.history.HistoryRepository;
 import com.marketboro.point.domain.reserves.Reserves;
 import com.marketboro.point.domain.reserves.repository.ReservesRepository;
 import com.marketboro.point.domain.reserves_history.ReservesHistoryRepository;
+import com.marketboro.point.dto.enums.HistoryType;
 import com.marketboro.point.dto.enums.ReservesStatus;
 import com.marketboro.point.dto.request.SaveReservesReq;
 import org.junit.jupiter.api.Disabled;
@@ -11,9 +13,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentMatcher;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -42,20 +46,22 @@ class ReservesServiceTest {
     @Mock
     private HistoryRepository historyRepository;
 
-
-    /*
-            Reserves savedReserves = reservesRepository.save(Reserves.of(saveReservesReq));
-        History history = History.of(saveReservesReq.getMemberId(), saveReservesReq.getAmount(), HistoryType.SAVE, false);
-        History savedHistory = historyRepository.save(history);
-
-        reservesHistoryRepository.save(ReservesHistory.of(savedReserves, savedHistory));
-     */
     @Test
-    @Disabled
+    @DisplayName("적립금 적립")
     public void savedReserves() {
+        // given
+        SaveReservesReq saveReservesReq = new SaveReservesReq("qwe5507", 1000L);
+        Reserves reserves = Reserves.of(saveReservesReq);
+        History history = History.of(saveReservesReq.getMemberId(), saveReservesReq.getAmount(), HistoryType.SAVE, false);
 
-        reservesService.savedReserves(new SaveReservesReq("qwe5507", 1000L));
+        // mocking
+        BDDMockito.given(reservesRepository.save(any(Reserves.class))).willReturn(reserves);
+        BDDMockito.given(historyRepository.save(any(History.class))).willReturn(history);
 
+        // when
+        reservesService.savedReserves(saveReservesReq);
+
+        // then
         verify(reservesRepository,times(1)).save(argThat(new IsReservesWillBeInserted()));
     }
 
@@ -63,7 +69,9 @@ class ReservesServiceTest {
         @Override
         public boolean matches(Reserves reserves) {
             return equals(reserves.getMemberId(),"qwe5507")
-                    && equals(reserves.getAmount(),1000L);
+                    && equals(reserves.getAmount(), 1000L)
+                    && equals(reserves.getBalance(), 1000L)
+                    && equals(reserves.getStatus(), ReservesStatus.UNUSED);
         }
         private boolean equals(Object actual,Object expected){
             return expected.equals(actual);
@@ -73,34 +81,46 @@ class ReservesServiceTest {
     @Test
     @DisplayName("적립금 합계 조회 시, 적립 내역이 없을 떄")
     public void 적립금_합계_조회_시_적립내역이_없을때() {
+        // given
         Long nowTime = LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
         String memberId = "1";
 
-        when(reservesRepository.findAllByMemberIdAndStatusAndNotExpired("1", ReservesStatus.UNUSED, 2L))
+        // mocking
+        when(reservesRepository.findAllByMemberIdAndStatusAndNotExpired("1", ReservesStatus.UNUSED, nowTime))
                 .thenReturn(new ArrayList<Reserves>());
 
-        assertThat(reservesService.getReservesTotal("1")).isEqualTo(0L);
+        // when
+        Long reservesTotal = reservesService.getReservesTotal(memberId, nowTime);
+
+        // then
+        assertThat(reservesTotal).isEqualTo(0L);
     }
 
     @Test
     @DisplayName("적립금 합계 조회 시, 적립 내역이 있을 때")
     public void 적립금_합계_조회_시_적립내역이_있을때() {
+        // given
         Long nowTime = LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
         String memberId = "1";
-
         Reserves reserves1 = Reserves.builder()
-                .memberId("1")
-                .amount(1L)
-                .build();
-
+                    .memberId("1")
+                    .balance(1L)
+                    .build();
         Reserves reserves2 = Reserves.builder()
-                .memberId("1")
-                .amount(2L)
-                .build();
+                    .memberId("1")
+                    .balance(2L)
+                    .build();
 
+        // mocking
         when(reservesRepository.findAllByMemberIdAndStatusAndNotExpired("1", ReservesStatus.UNUSED, nowTime))
                 .thenReturn(Arrays.asList(reserves1, reserves2));
 
-        assertThat(reservesService.getReservesTotal("1")).isEqualTo(3L);
+        //when
+        Long reservesTotal = reservesService.getReservesTotal("1", nowTime);
+
+        //then
+        assertThat(reservesTotal).isEqualTo(3L);
     }
+
+
 }
