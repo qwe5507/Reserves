6a5aa32d9c9a3cf505dc398d7e5f2ca0c4695414
qwe5507 <apvmffkdls@gmail.com>
2023-01-17 01:47:52 +0900

    chore : logback으로 jpa쿼리 로그저장되게 구현

--- src/main/java/com/marketboro/point/config/P6SpyFormatter.java
+++ src/main/java/com/marketboro/point/config/P6SpyFormatter.java
@@ -1,58 +1,58 @@
 package com.marketboro.point.config;
-
-import com.p6spy.engine.common.ConnectionInformation;
-import com.p6spy.engine.event.JdbcEventListener;
-import com.p6spy.engine.spy.P6SpyOptions;
-import com.p6spy.engine.spy.appender.MessageFormattingStrategy;
-import lombok.extern.slf4j.Slf4j;
-import org.hibernate.engine.jdbc.internal.FormatStyle;
-import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
-
-import java.sql.SQLException;
-
-@Component  // 1
-@Slf4j
-public class P6SpyFormatter extends JdbcEventListener implements MessageFormattingStrategy {
-
-    @Override // 2
-    public void onAfterGetConnection(ConnectionInformation connectionInformation, SQLException e) {
-        P6SpyOptions.getActiveInstance().setLogMessageFormat(getClass().getName());
-    }
-
-    @Override // 3
-    public String formatMessage(int connectionId, String now, long elapsed, String category, String prepared, String sql, String url) {
-        StringBuilder sb = new StringBuilder();
-
-        sb.append(category).append(" ").append(elapsed).append("ms");
-        if (StringUtils.hasText(sql)) {
-            // 하이라이트가 로그 저장시에 이상한 숫자로 표시됨
-//            sb.append(highlight(format(sql)));
-            sb.append(format(sql));
-        }
-        return sb.toString();
-//        return "#" + now + " | took " + elapsed + "ms | " + category + " | connection " + connectionId + "| url " + url + "\n" + prepared + "\n" + sql +";";
-    }
-    // 위의 주석 친 포매터가 이상한 데이터가 포함되서
-    private String format(String sql) {
-        if (isDDL(sql)) {
-            return FormatStyle.DDL.getFormatter().format(sql);
-        } else if (isBasic(sql)) {
-            return FormatStyle.BASIC.getFormatter().format(sql);
-        }
-        return sql;
-    }
-
-    private String highlight(String sql) {
-        return FormatStyle.HIGHLIGHT.getFormatter().format(sql);
-    }
-
-    private boolean isDDL(String sql) {
-        return sql.startsWith("create") || sql.startsWith("alter") || sql.startsWith("comment");
-    }
-
-    private boolean isBasic(String sql) {
-        return sql.startsWith("select") || sql.startsWith("insert") || sql.startsWith("update") || sql.startsWith("delete");
-    }
-
-}
+//
+//import com.p6spy.engine.common.ConnectionInformation;
+//import com.p6spy.engine.event.JdbcEventListener;
+//import com.p6spy.engine.spy.P6SpyOptions;
+//import com.p6spy.engine.spy.appender.MessageFormattingStrategy;
+//import lombok.extern.slf4j.Slf4j;
+//import org.hibernate.engine.jdbc.internal.FormatStyle;
+//import org.springframework.stereotype.Component;
+//import org.springframework.util.StringUtils;
+//
+//import java.sql.SQLException;
+//
+//@Component  // 1
+//@Slf4j
+//public class P6SpyFormatter extends JdbcEventListener implements MessageFormattingStrategy {
+//
+//    @Override // 2
+//    public void onAfterGetConnection(ConnectionInformation connectionInformation, SQLException e) {
+//        P6SpyOptions.getActiveInstance().setLogMessageFormat(getClass().getName());
+//    }
+//
+//    @Override // 3
+//    public String formatMessage(int connectionId, String now, long elapsed, String category, String prepared, String sql, String url) {
+//        StringBuilder sb = new StringBuilder();
+//
+//        sb.append(category).append(" ").append(elapsed).append("ms");
+//        if (StringUtils.hasText(sql)) {
+//            // 하이라이트가 로그 저장시에 이상한 숫자로 표시됨
+////            sb.append(highlight(format(sql)));
+//            sb.append(format(sql));
+//        }
+//        return sb.toString();
+////        return "#" + now + " | took " + elapsed + "ms | " + category + " | connection " + connectionId + "| url " + url + "\n" + prepared + "\n" + sql +";";
+//    }
+//    // 위의 주석 친 포매터가 이상한 데이터가 포함되서
+//    private String format(String sql) {
+//        if (isDDL(sql)) {
+//            return FormatStyle.DDL.getFormatter().format(sql);
+//        } else if (isBasic(sql)) {
+//            return FormatStyle.BASIC.getFormatter().format(sql);
+//        }
+//        return sql;
+//    }
+//
+//    private String highlight(String sql) {
+//        return FormatStyle.HIGHLIGHT.getFormatter().format(sql);
+//    }
+//
+//    private boolean isDDL(String sql) {
+//        return sql.startsWith("create") || sql.startsWith("alter") || sql.startsWith("comment");
+//    }
+//
+//    private boolean isBasic(String sql) {
+//        return sql.startsWith("select") || sql.startsWith("insert") || sql.startsWith("update") || sql.startsWith("delete");
+//    }
+//
+//}
--- src/test/java/com/marketboro/point/service/ReservesServiceTest.java
+++ src/test/java/com/marketboro/point/service/ReservesServiceTest.java
@@ -4,10 +4,13 @@ import com.marketboro.point.domain.history.History;
 import com.marketboro.point.domain.history.HistoryRepository;
 import com.marketboro.point.domain.reserves.Reserves;
 import com.marketboro.point.domain.reserves.repository.ReservesRepository;
+import com.marketboro.point.domain.reserves_history.ReservesHistory;
 import com.marketboro.point.domain.reserves_history.ReservesHistoryRepository;
 import com.marketboro.point.dto.enums.HistoryType;
 import com.marketboro.point.dto.enums.ReservesStatus;
 import com.marketboro.point.dto.request.SaveReservesReq;
+import com.marketboro.point.dto.request.UseReservesReq;
+import com.marketboro.point.exception.InvalidException;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -122,5 +125,91 @@ class ReservesServiceTest {
         assertThat(reservesTotal).isEqualTo(3L);
     }
 
+    @Test
+    @DisplayName("적립금 사용, 적립금이 부족할 때")
+    public void 적립금_사용_적립금이_부족할_떄() {
+        // given
+        UseReservesReq useReservesReq = new UseReservesReq("qwe5507", 10000L);
+        Long nowTime = LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
+
+        // mocking
+        when(reservesRepository.findAllByMemberIdAndStatusAndNotExpired(useReservesReq.getMemberId(), ReservesStatus.UNUSED, nowTime))
+                .thenReturn(Arrays.asList(Reserves.builder()
+                                        .memberId("qwe5507")
+                                        .balance(1000L)
+                                        .build()));
+        // when then
+        assertThrows(InvalidException.class, () -> reservesService.useReserves(useReservesReq, nowTime));
+    }
+
+    @Test
+    @DisplayName("적립금 사용 시, 5만원 적립 2번 후, 1만원 적립금 사용 시 오래된 적립금이 한번 사용 된다.")
+    public void 적립금_사용시_5만원적립_2번후_1만원_적립금_사용시_오래된_적립금이_한번_사용된다() {
+        // given
+        UseReservesReq useReservesReq = new UseReservesReq("qwe5507", 10000L); // 적립금 1만원 사용
+        Long nowTime = LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
+
+        List<Reserves> rerservesList = getRerservesList();
+        History history = History.of(useReservesReq.getMemberId(), useReservesReq.getAmount(), HistoryType.USE, false);
+
+        // mocking
+        when(reservesRepository.findAllByMemberIdAndStatusAndNotExpired(useReservesReq.getMemberId(), ReservesStatus.UNUSED, nowTime))
+                .thenReturn(rerservesList); // 적립금 10만원 보유
+//        BDDMockito.given(historyRepository.save(any(History.class))).willReturn(history);
+
+//        when(reservesHistoryRepository.save(ReservesHistory.of(reserves, savedHistory)))
+//                .thenReturn(getRerservesList()); // 적립금 10만원 보유
+
+        // when
+        reservesService.useReserves(useReservesReq, nowTime);
+
+        // then
+        verify(historyRepository,times(1)).save(argThat(new IsHistoryWillBeInserted()));
+        verify(reservesHistoryRepository,times(0)).save(argThat(new IsReservesHistoryWillBeInserted()));
+    }
+
+    private static class IsHistoryWillBeInserted implements ArgumentMatcher<History> {
+        @Override
+        public boolean matches(History history) {
+            return equals(history.getMemberId(),"qwe5507")
+                    && equals(history.getAmount(), 10000L)
+                    && equals(history.getType(), HistoryType.USE)
+                    && equals(history.isCanceled(), false);
+        }
+        private boolean equals(Object actual,Object expected){
+            return expected.equals(actual);
+        }
+    }
+
+    private static class IsReservesHistoryWillBeInserted implements ArgumentMatcher<ReservesHistory> {
+        @Override
+        public boolean matches(ReservesHistory reservesHistory) {
+            return equals(reservesHistory.getHistory().getMemberId(), "qwe5507")
+                    && equals(reservesHistory.getHistory().getAmount(), 10000L)
+                    && equals(reservesHistory.getHistory().getType(), HistoryType.USE)
+                    && equals(reservesHistory.getHistory().isCanceled(), false)
+                    && equals(reservesHistory.getReserves().getMemberId(), "qwe5507")
+                    && equals(reservesHistory.getReserves().getBalance(), 50000L);
+        }
+        private boolean equals(Object actual,Object expected){
+            return expected.equals(actual);
+        }
+    }
+
+    private List<Reserves> getRerservesList() {
+        Reserves reserves1 = Reserves.builder()
+                .memberId("qwe5507")
+                .balance(50000L)
+                .amount(50000L)
+                .build();
+
+        Reserves reserves2 = Reserves.builder()
+                .memberId("qwe5507")
+                .balance(50000L)
+                .amount(50000L)
+                .build();
+
+        return Arrays.asList(reserves1, reserves2);
+    }
 
 }
