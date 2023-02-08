c908d1023e560d9ae6fc3001684b6a672913b817
qwe5507 <apvmffkdls@gmail.com>
2023-02-08 22:04:50 +0900

    chore : 코드정리

--- src/test/java/com/marketboro/point/service/ReservesServiceTest.java
+++ src/test/java/com/marketboro/point/service/ReservesServiceTest.java
@@ -11,7 +11,6 @@ import com.marketboro.point.dto.enums.ReservesStatus;
 import com.marketboro.point.dto.request.SaveReservesReq;
 import com.marketboro.point.dto.request.UseReservesReq;
 import com.marketboro.point.exception.InvalidException;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -20,18 +19,15 @@ import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.Mockito.*;
 
@@ -65,20 +61,7 @@ class ReservesServiceTest {
         reservesService.savedReserves(saveReservesReq);
 
         // then
-        verify(reservesRepository,times(1)).save(argThat(new IsReservesWillBeInserted()));
-    }
-
-    private static class IsReservesWillBeInserted implements ArgumentMatcher<Reserves> {
-        @Override
-        public boolean matches(Reserves reserves) {
-            return equals(reserves.getMemberId(),"qwe5507")
-                    && equals(reserves.getAmount(), 1000L)
-                    && equals(reserves.getBalance(), 1000L)
-                    && equals(reserves.getStatus(), ReservesStatus.UNUSED);
-        }
-        private boolean equals(Object actual,Object expected){
-            return expected.equals(actual);
-        }
+        verify(reservesRepository, times(1)).save(argThat(new IsReservesWillBeInserted()));
     }
 
     @Test
@@ -106,13 +89,13 @@ class ReservesServiceTest {
         Long nowTime = LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
         String memberId = "1";
         Reserves reserves1 = Reserves.builder()
-                    .memberId("1")
-                    .balance(1L)
-                    .build();
+                .memberId("1")
+                .balance(1L)
+                .build();
         Reserves reserves2 = Reserves.builder()
-                    .memberId("1")
-                    .balance(2L)
-                    .build();
+                .memberId("1")
+                .balance(2L)
+                .build();
 
         // mocking
         when(reservesRepository.findAllByMemberIdAndStatusAndNotExpired("1", ReservesStatus.UNUSED, nowTime))
@@ -135,9 +118,9 @@ class ReservesServiceTest {
         // mocking
         when(reservesRepository.findAllByMemberIdAndStatusAndNotExpired(useReservesReq.getMemberId(), ReservesStatus.UNUSED, nowTime))
                 .thenReturn(Arrays.asList(Reserves.builder()
-                                        .memberId("qwe5507")
-                                        .balance(1000L)
-                                        .build()));
+                        .memberId("qwe5507")
+                        .balance(1000L)
+                        .build()));
         // when then
         assertThrows(InvalidException.class, () -> reservesService.useReserves(useReservesReq, nowTime));
     }
@@ -164,36 +147,8 @@ class ReservesServiceTest {
         reservesService.useReserves(useReservesReq, nowTime);
 
         // then
-        verify(historyRepository,times(1)).save(argThat(new IsHistoryWillBeInserted()));
-        verify(reservesHistoryRepository,times(0)).save(argThat(new IsReservesHistoryWillBeInserted()));
-    }
-
-    private static class IsHistoryWillBeInserted implements ArgumentMatcher<History> {
-        @Override
-        public boolean matches(History history) {
-            return equals(history.getMemberId(),"qwe5507")
-                    && equals(history.getAmount(), 10000L)
-                    && equals(history.getType(), HistoryType.USE)
-                    && equals(history.isCanceled(), false);
-        }
-        private boolean equals(Object actual,Object expected){
-            return expected.equals(actual);
-        }
-    }
-
-    private static class IsReservesHistoryWillBeInserted implements ArgumentMatcher<ReservesHistory> {
-        @Override
-        public boolean matches(ReservesHistory reservesHistory) {
-            return equals(reservesHistory.getHistory().getMemberId(), "qwe5507")
-                    && equals(reservesHistory.getHistory().getAmount(), 10000L)
-                    && equals(reservesHistory.getHistory().getType(), HistoryType.USE)
-                    && equals(reservesHistory.getHistory().isCanceled(), false)
-                    && equals(reservesHistory.getReserves().getMemberId(), "qwe5507")
-                    && equals(reservesHistory.getReserves().getBalance(), 50000L);
-        }
-        private boolean equals(Object actual,Object expected){
-            return expected.equals(actual);
-        }
+        verify(historyRepository, times(1)).save(argThat(new IsHistoryWillBeInserted()));
+        verify(reservesHistoryRepository, times(0)).save(argThat(new IsReservesHistoryWillBeInserted()));
     }
 
     private List<Reserves> getRerservesList() {
@@ -210,6 +165,50 @@ class ReservesServiceTest {
                 .build();
 
         return Arrays.asList(reserves1, reserves2);
+    }
+
+    private static class IsReservesWillBeInserted implements ArgumentMatcher<Reserves> {
+        @Override
+        public boolean matches(Reserves reserves) {
+            return equals(reserves.getMemberId(), "qwe5507")
+                    && equals(reserves.getAmount(), 1000L)
+                    && equals(reserves.getBalance(), 1000L)
+                    && equals(reserves.getStatus(), ReservesStatus.UNUSED);
+        }
+
+        private boolean equals(Object actual, Object expected) {
+            return expected.equals(actual);
+        }
+    }
+
+    private static class IsHistoryWillBeInserted implements ArgumentMatcher<History> {
+        @Override
+        public boolean matches(History history) {
+            return equals(history.getMemberId(), "qwe5507")
+                    && equals(history.getAmount(), 10000L)
+                    && equals(history.getType(), HistoryType.USE)
+                    && equals(history.isCanceled(), false);
+        }
+
+        private boolean equals(Object actual, Object expected) {
+            return expected.equals(actual);
+        }
+    }
+
+    private static class IsReservesHistoryWillBeInserted implements ArgumentMatcher<ReservesHistory> {
+        @Override
+        public boolean matches(ReservesHistory reservesHistory) {
+            return equals(reservesHistory.getHistory().getMemberId(), "qwe5507")
+                    && equals(reservesHistory.getHistory().getAmount(), 10000L)
+                    && equals(reservesHistory.getHistory().getType(), HistoryType.USE)
+                    && equals(reservesHistory.getHistory().isCanceled(), false)
+                    && equals(reservesHistory.getReserves().getMemberId(), "qwe5507")
+                    && equals(reservesHistory.getReserves().getBalance(), 50000L);
+        }
+
+        private boolean equals(Object actual, Object expected) {
+            return expected.equals(actual);
+        }
     }
 
 }
